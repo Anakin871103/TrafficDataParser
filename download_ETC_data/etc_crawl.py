62a36832c209e9029e1dbf85792dead7b97f4745
Wang, Hsuan Chih (王軒至) <transport.hcwang@gmail.com>
2023-02-22 09:21:17 +0800

    20230222

---
+++ download_ETC_data/etc_crawl.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 20:36:53 2022
+@author: Leo Wang
+"""
+
+import os 
+import requests as request
+import tarfile
+import datetime
+import pandas as pd
+
+COLUMN_NAMES = {'M03A': ['TimeInterval', 'GantryID', 'Direction', 'VehicleType', 'Volume'],
+               'M05A': ['TimeInterval', 'GantryFrom', 'GantryTo', 'VehicleType', 'SpaceMeanSpeed', 'Volume'],
+               'M06A': ['VehicleType', 'DetectionTime_O', 'GantryID_O', 'DetectionTime_D', 'GantryID_D', 'TripLength', 'TripEnd', 'TripInformation'],
+               'M08A': ['TimeInterval', 'GantryFrom', 'GantryTo', 'VehicleType', 'Volume']}
+
+BASE_PATH = 'D:/freewayData/ETC_2021/'
+
+def change_directory(path: str):
+    os.chdir(path)
+
+def download_data():
+
+    firstDate = input("firstDate = MMDD")
+    lastDate = input("lastDate = MMDD")
+    dataType = input('type = ? (EX. M06)') + 'A'
+
+    first_date = '2021' + firstDate + '0000'
+    last_date = '2021' + lastDate + '2355'
+
+    start = datetime.datetime.strptime(first_date, '%Y%m%d%H%M')
+    end = datetime.datetime.strptime(last_date, '%Y%m%d%H%M')
+
+    while start <= end:
+
+        year = datetime.datetime.strftime(start, "%Y")
+        month = datetime.datetime.strftime(start, "%m")
+        day = datetime.datetime.strftime(start, "%d")
+        hour = datetime.datetime.strftime(start, "%H%M")
+
+        #url = "https://tisvcloud.freeway.gov.tw/history/_vd/" + year + month + date + "/vd_value5_" + hour + ".xml.gz"
+        download_ETC_fileName = dataType + "_" + year + month + day + ".tar.gz"
+        url = "https://tisvcloud.freeway.gov.tw/history/TDCS/" + dataType + "/" + download_ETC_fileName
+        html = request.get(url)
+
+        if html.status_code == 200:
+            #downloadPath = "C:/Users/user/ETC/"
+            downloadPath = "C:/Users/WangRabbit/Documents/GitHub/ETC_FreewayAccidentAnalysis/data/Reference-Data/"
+            # 切換目標資料夾: 從當前資料夾至預備儲存資料的資料夾
+            os.chdir(downloadPath)
+            # 確認month的資料夾是否存在
+            if not os.path.exists(month):
+                os.makedirs(month) #新增month
+            os.chdir(month)
+
+            with open(download_ETC_fileName, "wb") as f:
+                f.write(html.content)
+
+            #解壓縮檔案
+            # g_file = gzip.GzipFile(mode='rb', fileobj=open(download_ETC_fileName, 'rb'))
+            # newFileName = download_ETC_fileName.replace(".tar.gz", "")
+
+            # open file
+            file = tarfile.open(download_ETC_fileName)
+            # extracting file
+            file.extractall('.')
+            file.close()
+
+            # open(newFileName, "wb+").write(g_file.read())
+            # g_file.close()
+
+            #刪除原本的壓縮檔
+            if os.path.exists(download_ETC_fileName):
+                os.remove(download_ETC_fileName)
+
+            print(month + day + " is downloaded")
+
+        start = start + datetime.timedelta(days=1)
+
+
+def combine_data():
+
+    dataType = input('type = ? (ex. M06)') + 'A'
+    #default 20210101~20211231
+    first_date = input('FirstDate YYYY-MMDD-HHMM: ') #'2021-0101-0000'
+    last_date = input('EndDate YYYY-MMDD-HHMM: ') #'2021-0131-2355'
+    
+    change_directory(path=BASE_PATH)
+
+    start = datetime.datetime.strptime(first_date, '%Y-%m%d-%H%M')
+    end = datetime.datetime.strptime(last_date, '%Y-%m%d-%H%M')
+
+    while start <= end: #day level
+
+        #建立一個dataframe結構
+        dfAppend = pd.DataFrame(columns=COLUMN_NAMES[dataType])
+
+        year = datetime.datetime.strftime(start, "%Y")
+        month = datetime.datetime.strftime(start, "%m")
+        day = datetime.datetime.strftime(start, "%d")
+
+        startHour = '00'
+        startMin = '00'
+        endHour = '23'
+        endMin = '55'
+
+        startTime = datetime.datetime.strptime(startHour + startMin, '%H%M')
+        endTime = datetime.datetime.strptime(endHour + endMin, '%H%M')
+
+        while startTime <= endTime:
+
+            hour = datetime.datetime.strftime(startTime, "%H")
+            min = datetime.datetime.strftime(startTime, "%M")
+            second = '00'
+
+            # 到M0X資料夾
+            change_directory(path=BASE_PATH + month + '/' + dataType + '/' + year + month + day + '/' + hour)
+
+            fileName = 'TDCS_' + dataType + '_' + year + month + day + '_' + hour + min + second
+
+            try:
+                dfTemp = pd.read_csv(fileName + ".csv") #讀取csv
+                dfTemp.columns = COLUMN_NAMES[dataType] #存取欄位名稱
+                dfAppend = dfAppend.append(dfTemp, ignore_index=True) #append
+                print(f"Combined {dataType} -> {year}-{month}-{day}-{hour}-{min}-{second} successfully!")
+            except pd.errors.EmptyDataError as e1:
+                #例外處理: 內容為空
+                print(f"Ignore {dataType} -> {year}-{month}-{day}-{hour}-{min}-{second} since it is empty")
+            except FileNotFoundError as e2:
+                #例外處理: 找不到檔案
+                print(f"Ignore {dataType} -> {year}-{month}-{day}-{hour}-{min}-{second} since file is not found!")
+
+
+            startTime = startTime + datetime.timedelta(minutes=5)
+
+        change_directory(path=BASE_PATH + '/afterCombination/' + dataType + '/2021年/' + str(int(month)) + '月/')
+
+        # 將dfAppend轉成csv
+        dfAppend.to_csv(day + '日.csv')
+        print(f"Create {day + '日.csv'} successfully!")
+
+        start = start + datetime.timedelta(days=1)
+
+    print("COMBINATION TASKS DONE!")
+
+
+if __name__ == "__main__":
+    #Program Start
+    while True:
+        mode = input("Mode 1 = Download Data / Mode 2 = Combine Data. Please Enter Mode: ")
+        if mode == '1':
+            print("Download Data!")
+            download_data()
+            break
+        elif mode == '2':
+            print("Combine Data!")
+            combine_data()
+            break
+        else:
+            print(f"Unkown Mode = {mode} Only accept 1 or 2")
+
+#End
+print("ALL TASKS DONE")
+
---
+++ download_weather_data/weather_crawl.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  3 10:01:03 2023
+@author: user
+"""
+from selenium import webdriver
+import requests as request 
+import os 
+import csv
+import datetime
+import time
+from bs4 import BeautifulSoup
+
+COLS = ['測站時間', '測站氣壓', '海平面氣壓', '氣溫', '露點溫度', '相對溼度',
+        '風速', '風向', '最大陣風', '最大陣風風向', '降水量', '降水時數', '日照時數',
+        '全天空日射量', '能見度', '紫外線指數', '總雲量']
+
+stations = []
+countyNameList = []
+
+# os.chdir('C://python')
+with open('測站網址(1).csv', newline='', encoding='utf-8') as csvfile:
+    SKIPROWS = 700  #跳到指定的ROW: 選擇從哪一個測站開始下載
+    rows = csv.reader(csvfile, skipinitialspace=True)
+    #跳到指定的ROW
+    for i in range(SKIPROWS):
+        next(rows)
+    for row in rows:
+        stations.append([row[0], row[1], row[6], row[-2], row[-1]])
+        countyNameList.append(row[0])
+
+first = '2021-01-01'
+last = '2021-12-31'
+
+#切換目標資料夾: 從當前資料夾至預備儲存資料的資料夾
+os.chdir('../data/Reference-Data/Central Weather Bureau/天氣資料庫/')
+#新建各縣市資料夾
+for countyName in countyNameList:
+    if not os.path.exists(countyName): #確認縣市的資料夾是否存在
+        os.makedirs(countyName)
+
+#iterate each station
+for station in stations[1:]: #ignore row 0: column names
+
+    def change_directory(directoryName: str):
+        try:
+            os.chdir(directoryName)
+        except FileNotFoundError as e:
+            print("The system cannot find the path specified!!!")
+            raise e
+
+    def check_html_status(code: int):
+        if code == 200:
+            return True
+        else:
+            return False
+
+    def writeToCSVfile(exportFileName: str, mode: str):
+        try:
+            with open(exportFileName, mode, newline='', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow(COLS)
+                for i in totalData:
+                    writer.writerow(i)
+                print(exportFileName + ' is done!')
+        except PermissionError as e:
+            print("permission error!")
+            raise e
+
+        return 0
+
+    totalData = []
+
+    county = station[0]
+    stationName = station[1]
+    stationAddress = station[2]
+    url1 = station[3]
+    url2 = station[4]
+
+    #切換至指定縣市資料夾
+    change_directory(county)
+    #時間校正
+    startDate = datetime.datetime.strptime(first, '%Y-%m-%d')
+    endDate = datetime.datetime.strptime(last, '%Y-%m-%d')
+
+    while startDate <= endDate:
+
+        year = datetime.datetime.strftime(startDate, "%Y")
+        month = datetime.datetime.strftime(startDate, "%m")
+        date = datetime.datetime.strftime(startDate, "%d")
+
+        url = url1 + year +'-' + month + '-' + date + url2
+        html = request.get(url)
+
+        if check_html_status(html.status_code):
+
+            soup = BeautifulSoup(html.text, 'html.parser')
+            tag_table = soup.find(id='MyTable')
+            rows = tag_table.findAll('tr')
+
+            check = 0
+            for row in rows:
+                if check < 3: #跳過html網頁內容的前3行?
+                    check+=1
+                else:
+                    text_info = row.findAll(['td'])
+                    result = []
+                    for sub_text in text_info: #存取資訊填入list?
+                        info = sub_text.get_text()
+                        info = info.strip()
+                        result.append(info)
+
+                    hour = result[0]
+
+                    if result[0] == '24':
+                        #沒有24:00:00 -> 切換成下一天的00:00:00
+                        result[0] = datetime.datetime.strftime(startDate + datetime.timedelta(days=1), "%Y-%m-%d") \
+                                    + ' 00:00:00'
+                    else:
+                        result[0] = datetime.datetime.strftime(startDate, "%Y-%m-%d") \
+                                    + ' ' + hour + ':00:00'
+
+                    totalData.append(result)
+
+        else:
+            print("網頁無法正常存取!")
+            print(1/0) #拋出例外
+
+        #proceed to the next day
+        startDate = startDate + datetime.timedelta(days=1)
+
+    exportFileName = stationName + "_" + stationAddress +'.csv'
+    #export csv file
+    writeToCSVfile(exportFileName=exportFileName, mode='w')
+    #資料夾跳回上層
+    os.chdir('../')
+
+        
+print("All Tasks DONE!")
+os.system("pause")
+    
+    
