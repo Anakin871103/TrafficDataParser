fb60801ced7d194482a01f41a2530bd05d4c6339
Hsuan-Chih, Wang (王軒至) <53686476+HsuanChih-Wang@users.noreply.github.com>
2023-04-12 10:54:44 +0800

    20230412

--- TrafficDataParser/CSVParser.py
+++ TrafficDataParser/CSVParser.py
@@ -22,54 +22,10 @@ class CSVParser():
         self.CSVFileContent = []
         self.CSVFileColumnNames = 0
 
-    def __readCSVfile(self, method):
-        try:
-            with open(os.path.join(self.fileRoute, self.fileName), 'r', encoding='UTF-8') as file:
-                if method == 'dict':
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        self.CSVFileContent.append(dict(row))  # Record in dictionary form
-                elif method == 'normal':
-                    reader = csv.reader(file)
-                    for row in reader:
-                        self.CSVFileContent.append(row) #Record in list form
-                    self.CSVFileColumnNames = self.CSVFileContent[0]  # first row is column names -> save them to class property
-                    self.CSVFileContent.remove(self.CSVFileContent[0])  # remove first row -> column names
-
-        except FileNotFoundError:
-            print(f'file directory: {os.path.join(self.fileRoute, self.fileName)} not found!')
-            raise FileNotFoundError
-        #raise FileNotFoundError
-
-    def __writeCSVfile(self, method: str, newFileName: str, data):
-        if method == 'normal':
-            with open(os.path.join(self.fileRoute, newFileName), 'w', encoding='UTF-8', newline='') as file:
-                writer = csv.writer(file)  # create the csv writer
-                writer.writerow(data)  # write a row to the csv file
-        elif method == 'dict':
-            with open(os.path.join(self.fileRoute, newFileName), 'w', encoding='UTF-8', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=colunmName)  # create the csv writer
-                writer.writerows(data)  # "data" -> a list contains multiple data
-        else:
-            print("Method does not exist!")
 
     def readCSVfile(self, encoding, **kwargs):
-
-        #try:
         self.CSVFileContent = pd.read_csv(filepath_or_buffer=self.path, encoding=encoding, **kwargs)
         self.CSVFileColumnNames = self.CSVFileContent.columns.values.tolist()
-
-        # except FileNotFoundError:
-        #     print(f'file : {os.path.join(self.fileRoute, self.fileName)} not found!')
-        #     raise FileNotFoundError
-        #
-        # except UnicodeDecodeError:
-        #     print(f'file directory: {os.path.join(self.fileRoute, self.fileName)} can not be decoded by "utf-8"!'
-        #           f'Please check encoded type of the file and specify decoded type!')
-        #     raise UnicodeDecodeError
-
-    # def writeCSVfile(self, newfileName, data, **kwargs):
-    #     data.to_csv(newfileName, **kwargs)
 
     def getCSVfileContent(self):
         return self.CSVFileContent
