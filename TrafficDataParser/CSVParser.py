150a9baab96c6d38a3a76ca916b5c1cd3d57283e
Hsuan-Chih, Wang (王軒至) <53686476+HsuanChih-Wang@users.noreply.github.com>
2023-05-14 12:27:28 +0800

    Update CSVParser.py

--- TrafficDataParser/CSVParser.py
+++ TrafficDataParser/CSVParser.py
@@ -44,11 +44,10 @@ class CSVParser():
             print(f"columnName: {columnName} is not existed in the columnNameList {self.CSVFileColumnNames}")
             raise ValueError
         return index
-    
-    # this is for the "skiprows" parameters in pd.read(). 
-    # It will generate a set of number of "rows" that should be skipped accodrding to user's input. 
 
     def generate_skiprows(self, startRow: int, endRow: int) -> np.ndarray:
+        # this is for the "skiprows" parameters in pd.read(). 
+        # It will generate a set of number of "rows" that should be skipped accodrding to user's input. 
         originalNumberOfRows = self.get_CSVFileOriginalNumberOfRows()
         allRows = np.array([i for i in np.arange(originalNumberOfRows+1)])
         wantedRows = np.array([0] + [i for i in np.arange(startRow, endRow+1)])  # [0] -> row of column names
